4fecd47ca466b390b649a51ff783d40300b7a30b
Joshim Uddin <joshimcse@gmail.com>
2020-01-06 19:33:30 +0600

    create a store using context

---
+++ src/store/useGlobalState.js
@@ -0,0 +1,24 @@
+import { useReducer } from 'react';
+import storage from 'local-storage-fallback';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'TOGGLE_DARK_MODE':
+      return {
+        isDark: !state.isDark,
+      };
+    default: {
+      return state;
+    }
+  }
+};
+
+const useGloablState = () => {
+  const [state, dispatch] = useReducer(reducer, {
+    isDark: true,
+  });
+
+  return { state, dispatch };
+};
+
+export default useGloablState;
